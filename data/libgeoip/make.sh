d893fcfd7fef2413dc6019f27f08ab08e2a04138
anrugas <anrugas@localhost>
2015-07-01 11:42:18 -0500

    libgeoip: Update to version 1.6.5

--- data/libgeoip/make.sh
+++ data/libgeoip/make.sh
@@ -1,10 +1,8 @@
 pkg: mkdir -p /usr/local/lib
 pkg: mkdir -p /usr/local/bin
-pkg: mkdir -p /etc
-pkg: mkdir -p /usr/local/share
+pkg: mkdir -p /usr/local/share/GeoIP
 cp -a "$(PKG_DEST_ _libgeoip)"/usr/local/lib/*.dylib "${PKG_DEST}"/usr/local/lib
 cp -ra "$(PKG_DEST_ _libgeoip)"/usr/local/bin/* "${PKG_DEST}"/usr/local/bin
-cp -ra "$(PKG_DEST_ _libgeoip)"/usr/local/share/* "${PKG_DEST}"/usr/local/share
-cp -ra "$(PKG_DEST_ _libgeoip)"/etc/* "${PKG_DEST}"/etc
+cp -ra "$(PKG_DEST_ _libgeoip)"/usr/local/share/GeoIP/GeoIP.dat "${PKG_DEST}"/usr/local/share/GeoIP 
 
 cp "${PKG_DATA}"/geoipupdt.sh "${PKG_DEST}"/usr/local/bin
