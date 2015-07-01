599c58eefcf572c25fd6558c29e6f57fcc7c9189
anrugas <anrugas@localhost>
2015-07-01 12:12:22 -0500

    libgpg-error: Update to version 1.19

--- data/libgpg-error/make.sh
+++ data/libgpg-error/make.sh
@@ -1,5 +1,7 @@
 pkg:setup
 pkg:configure
-cp src/syscfg/lock-obj-pub.arm-apple-darwin.h src/syscfg/lock-obj-pub.darwin9.h
+
+cp src/syscfg/lock-obj-pub.arm-apple-darwin.h src/syscfg/lock-obj-pub.arm-apple-darwin9.h
+
 make
 pkg:install
